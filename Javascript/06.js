dfe89725bf0fdbd303d3edf21eb7c08f1ebd2bfa
Sabino Capingana <77695364+Capingana@users.noreply.github.com>
2024-02-08 22:58:53 +0100

    map

---
+++ Javascript/06.js
@@ -0,0 +1,6 @@
+// Manipulando objectos com JAVASCRIPT
+
+const fullname=()=>{
+    yield"name";
+    console.log("TESTE DE FIDELIDADE E MUITO MAIS...");
+}
---
+++ Javascript/07.js
@@ -0,0 +1,7 @@
+const names=['sabino','capingana','jose','pedro','antunes','laurindo'];
+
+let nomesvistos=names.map((nome,posicao)=>{
+console.log(nome.toLocaleUpperCase()+" = "+nome.charAt(0));
+
+
+});
