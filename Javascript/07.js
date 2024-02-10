3a202f57a1d323d18787ef8ba392fde7cd7e4f19
Sabino Capingana <77695364+Capingana@users.noreply.github.com>
2024-02-11 00:17:18 +0100

    yes

--- Javascript/07.js
+++ Javascript/07.js
@@ -1,7 +1,2 @@
-const names=['sabino','capingana','jose','pedro','antunes','laurindo'];
-
-let nomesvistos=names.map((nome,posicao)=>{
-console.log(nome.toLocaleUpperCase()+" = "+nome.charAt(0));
-
-
-});
+let names=['A','E','I','O','U'];
+console.log(names.length);
